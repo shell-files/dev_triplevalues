d84282af11ddb40c5e290b47fd1a5d4700757050
shellfolders <232290329+shellfolders@users.noreply.github.com>
2026-04-24 13:05:26 +0900

    FastAPI 테스트

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from settings import settings
 
+app = FastAPI(root_path="/tv", servers=[
+  {"url": "/tv", "description": "API 기본 서버"}
+])
 
-if __name__ == "__main__":
-    main()
+origins = ["http://localhost", settings.host_ip]
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+  return {"msg": "Triple Values!"}
