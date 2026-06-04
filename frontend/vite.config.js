375d06d967c6cd1df94319388b45f42a31a8fd2b
unknown <nara1743@gmail.com>
2026-06-04 15:07:45 +0900

    [FE] `vite.config.js` 알리아스 경로 수정 (#11)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -15,11 +15,17 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+      '@errors': fileURLToPath(new URL('./src/homes/errors', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
       '@styles': fileURLToPath(new URL('./src/styles', import.meta.url)),
+      '@gates': fileURLToPath(new URL('./src/homes/gates', import.meta.url)),
+      '@logins': fileURLToPath(new URL('./src/homes/logins', import.meta.url)),
+      '@mains': fileURLToPath(new URL('./src/homes/mains', import.meta.url)),
       '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
       '@hooks': fileURLToPath(new URL('./src/hooks', import.meta.url)),
       '@utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
+      '@admin': fileURLToPath(new URL('./src/homes/admin', import.meta.url)),
+      '@partners': fileURLToPath(new URL('./src/homes/partners', import.meta.url)),
     }
   }
 })
