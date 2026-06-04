420b7c69c7b968f0025ccea8e3193eae35291809
unknown <nara1743@gmail.com>
2026-06-04 17:33:39 +0900

    [FE] `vite.config.js` 알리아스 경로 재수정 (#11)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import { fileURLToPath, URL } from 'url'
 
-// https://vite.dev/config/
+// [https://vite.dev/config/](https://vite.dev/config/)
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: "/",
   server: {
     host: '0.0.0.0',
     port: 80,
     allowedHosts: ['react', 'tv.weareithero.cloud', 'tv.myapp.com'],
-    // host: true,
   },
   resolve: {
     alias: {
@@ -23,9 +23,6 @@ export default defineConfig({
       '@mains': fileURLToPath(new URL('./src/homes/mains', import.meta.url)),
       '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
       '@hooks': fileURLToPath(new URL('./src/hooks', import.meta.url)),
-      '@utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
-      '@admin': fileURLToPath(new URL('./src/homes/admin', import.meta.url)),
-      '@partners': fileURLToPath(new URL('./src/homes/partners', import.meta.url)),
     }
   }
 })
